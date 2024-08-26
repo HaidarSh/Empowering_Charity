fa70072197a9cbc1ba5bc3068957150d9a8185db
haidarimad <haidarshokor2@gmail.com>
2024-08-26 16:26:14 +0300

    added

--- client_side/vite.config.js
+++ client_side/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   optimizeDeps: {
     include: ["ethers"],
   },
-  // build: {
+  build: {
   //   rollupOptions: {
   //     output: {
   //       manualChunks(id) {
@@ -23,6 +23,6 @@ export default defineConfig({
   //       },
   //     },
   //   },
-  //   chunkSizeWarningLimit: 1000,
-  // },
+    chunkSizeWarningLimit: 5000,
+  },
 });
