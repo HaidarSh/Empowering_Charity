826b05e3ea87f10809d24ab221baf0180f18ce9e
haidarimad <haidarshokor2@gmail.com>
2024-09-08 23:47:47 +0300

    update 11 home page

--- client_side/src/pages/Home.jsx
+++ client_side/src/pages/Home.jsx
@@ -151,16 +151,16 @@ export default function Home() {
           </div>
           <h1 className="text-xl text-[var(--text-color)] mb-10"></h1>
           <div className="flex flex-wrap gap-4 items-center justify-center">
-          <div className="charityphoto charityphoto-medium flex w-full md:w-1/2 lg:w-1/2 xl:w-1/3 items-center justify-center">
-            <CharityPhotosStyleThree imageSRC={seven} />
+            <div className="charityphoto charityphoto-medium flex w-full md:w-1/2 lg:w-1/2 xl:w-1/3 items-center justify-center">
+              <CharityPhotosStyleThree imageSRC={seven} />
+            </div>
+            <div className="charityphoto charityphoto-medium flex w-full md:w-1/2 lg:w-1/2 xl:w-1/3 items-center justify-center">
+              <CharityPhotosStyleThree imageSRC={eight} />
+            </div>
+            <div className="charityphoto charityphoto-medium flex w-full md:w-1/2 lg:w-1/2 xl:w-1/3 items-center justify-center">
+              <CharityPhotosStyleThree imageSRC={third} />
+            </div>
           </div>
-          <div className="charityphoto charityphoto-medium flex w-full md:w-1/2 lg:w-1/2 xl:w-1/3 items-center justify-center">
-            <CharityPhotosStyleThree imageSRC={eight} />
-          </div>
-          <div className="charityphoto charityphoto-medium flex w-full md:w-1/2 lg:w-1/2 xl:w-1/3 items-center justify-center">
-            <CharityPhotosStyleThree imageSRC={third} />
-          </div>
-        </div>
         </div>
 
         <hr className="border-t-2 border-[#A18167]" />
