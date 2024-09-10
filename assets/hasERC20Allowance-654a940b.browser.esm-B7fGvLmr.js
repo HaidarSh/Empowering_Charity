6fff639ffcca84d4a22a6ae63e5c34bc471dafec
haidarimad <haidarshokor2@gmail.com>
2024-09-10 17:18:49 +0300

    Updates

--- assets/hasERC20Allowance-654a940b.browser.esm-B7fGvLmr.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index-BITOK-ay.js","assets/index-byXBim9C.css"])))=>i.map(i=>d[i]);
-import{c,a1 as _}from"./index-BITOK-ay.js";async function f(e,a,t){const n=e.getProvider(),r=(await c(async()=>{const{default:o}=await import("./index-BITOK-ay.js").then(l=>l.ec);return{default:o}},__vite__mapDeps([0,1]))).default,s=new _(n,a,r,{},e.storage),d=await e.getSignerAddress(),i=e.address;return(await s.read("allowance",[d,i])).gte(t)}export{f as h};
